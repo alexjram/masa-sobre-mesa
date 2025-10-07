d816234e00a775ec80ab5950c8dba0afd12cbb77
Alexis Ramírez <alexis.jrm.0395@gmail.com>
2025-10-07 19:07:31 -0400

    started making changes for desktop

---
+++ assets/js/components/Carousel.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
+import type { Food as FoodType } from '../App'
+import Food from './Food'
+import LeftArrow from './LeftArrow'
+import RightArrow from './RightArrow'
+
+interface Props {
+	foods: FoodType[]
+}
+
+export default function Carousel({ foods }: Props) {
+	const [emblaRef, emblaApi] = useEmblaCarousel({
+		align: 'start',
+		loop: true
+	})
+
+	const handleLeftButton = () => {
+		emblaApi?.scrollPrev()
+	}
+	const handleRightButton = () => {
+		emblaApi?.scrollNext()
+	}
+
+	return (
+		<div className='relative h-full w-full'>
+			<div className='overflow-hidden h-full' ref={emblaRef}>
+				<button className='absolute left-4 top-1/2 translate-y-[-50%] z-10 hidden lg:block' onClick={handleLeftButton}>
+					<LeftArrow />
+				</button>
+				<button className='absolute right-4 top-1/2 translate-y-[-50%] z-10 hidden lg:block' onClick={handleRightButton}>
+					<RightArrow />
+				</button>
+				<div className='flex'>
+					{foods.map((f, i) => (<Food food={f} key={i} />))}
+				</div>
+			</div>
+		</div>
+	)
+}
--- assets/js/views/MenuItems.tsx
+++ assets/js/views/MenuItems.tsx
@@ -2,10 +2,10 @@ import React, { useRef, useState } from 'react'
 import MenuIcon from '../components/MenuIcon'
 import Menu from './Menu'
 import Scrollable from '../components/Scrollable'
-import LeftArrow from '../components/LeftArrow'
-import RightArrow from '../components/RightArrow'
 import Food from '../components/Food'
 import type { Food as FoodType } from '../App'
+import useIsMobile from '../hooks/useIsMobile'
+import Carousel from '../components/Carousel'
 
 interface Props {
 	name: string
@@ -16,6 +16,7 @@ interface Props {
 export default function MenuItems({ name, foods, onChange, types }: Props) {
 	const [showMenu, setShowMenu] = useState(false)
 	const contentRef = useRef<HTMLDivElement>(null)
+	const isMobile = useIsMobile(1024)
 	const handleOpenMenu = () => {
 		if (contentRef.current) {
 			contentRef.current.scrollTo({ top: 0, behavior: 'instant' })
@@ -29,27 +30,9 @@ export default function MenuItems({ name, foods, onChange, types }: Props) {
 		onChange(type)
 		setShowMenu(false)
 	}
-	const handleLeftButton = () => {
-		if (contentRef.current) {
-			contentRef.current.scrollBy({ left: -100, behavior: 'smooth' })
-		}
-	}
-	const handleRightButton = () => {
-		if (contentRef.current) {
-			contentRef.current.scrollBy({ left: 100, behavior: 'smooth' })
-		}
-	}
 	return (
 		<div className="w-full h-full relative">
 			<div className="absolute top-0 left-0 w-full h-full flex flex-col justify-stretch">
-				<button className='absolute left-4 top-1/2 translate-y-[-50%] z-10 hidden lg:block' onClick={handleLeftButton}>
-					<LeftArrow />
-				</button>
-				<button className='absolute right-4 top-1/2 translate-y-[-50%] z-10 hidden lg:block' onClick={handleRightButton}>
-					<RightArrow />
-				</button>
-
-
 				<div className="flex-shrink-0 flex-grow-0 bg-white py-2 lg::hidden">
 					{!showMenu ? (<h2 className="text-center text-2xl flex justify-center">
 						<span className="mr-3">{name}</span>
@@ -60,16 +43,20 @@ export default function MenuItems({ name, foods, onChange, types }: Props) {
 						</h2>
 					)}
 				</div>
-				<Scrollable showMenu={showMenu} ref={contentRef}>
-					<ul className="flex flex-col lg:flex-row lg:h-full whitespace-nowrap">
-						{foods.map((food, index) => (
-							<Food food={food} key={index} />
-						))}
-					</ul>
-					<div className={`absolute w-full h-full bottom-0 top-0 ${showMenu ? 'z-auto' : '-z-10'}`}>
-						<Menu types={types} onChange={handleMenuChange} visible={showMenu} isInternal={true} />
-					</div>
-				</Scrollable>
+				{isMobile ? (
+					<Scrollable showMenu={showMenu} ref={contentRef}>
+						<ul className="flex flex-col lg:flex-row lg:h-full whitespace-nowrap">
+							{foods.map((food, index) => (
+								<Food food={food} key={index} />
+							))}
+						</ul>
+						<div className={`absolute w-full h-full bottom-0 top-0 ${showMenu ? 'z-auto' : '-z-10'}`}>
+							<Menu types={types} onChange={handleMenuChange} visible={showMenu} isInternal={true} />
+						</div>
+					</Scrollable>
+				) : (
+					<Carousel foods={foods} />
+				)}
 
 			</div>
 
