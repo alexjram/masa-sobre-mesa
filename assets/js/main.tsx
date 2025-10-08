c2f5f8bff10de670a8d3d3139571375851940ad0
Alexis Ramírez <alexis.jrm.0395@gmail.com>
2025-10-07 21:22:51 -0400

    remove loggin code

--- assets/js/main.tsx
+++ assets/js/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client"
 import App from './App'
 
 const root = document.getElementById('react-root')
-alert(JSON.stringify(globalTerms))
 if (root) {
 	createRoot(root).render(<App types={globalTerms} />)
 }
