cfea332a592b4598dabf0fa8692966ebea8745fb
Alexis Ramírez <alexis.jrm.0395@gmail.com>
2025-10-08 22:38:08 -0400

    things for mobile i guess

--- assets/js/components/Food.tsx
+++ assets/js/components/Food.tsx
@@ -11,7 +11,7 @@ export default function Food({ food }: Props) {
 			<img src={food.image2} alt={food.name} className="aspect-square object-cover group-hover/food:opacity-100 opacity-0 ease-in-out duration-300 absolute top-0 left-0" />
 
 			<div className="absolute w-full bottom-0 px-3 pt-6 group-hover/food:py-7 pb-3 -translate-x-1/4 md:translate-x-0 group-hover/food:translate-x-0 ease-in-out duration-300">
-				<div className='pink-blob-bg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[130%] h-full' />
+				<div className='pink-blob-bg absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[130%] h-full z-10' />
 				<h3 className="text-center uppercase lg:text-xl mx-auto w-[45%] whitespace-normal">{food.name}</h3>
 				<p className="text-center max-h-0 overflow-hidden group-hover/food:max-h-80 ease-in-out duration-200 px-5 group-hover/food:mt-5 lg:px-[12%] whitespace-normal text-[13px]">{food.desc}</p>
 			</div>
